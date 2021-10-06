5458a1178dac470995eb4d48aedada353fa75bbe
Mohibul-alom <mohibulalom@outlook.es>
2021-10-06 17:36:36 +0200

    FEAT: added user crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 
 @Module({

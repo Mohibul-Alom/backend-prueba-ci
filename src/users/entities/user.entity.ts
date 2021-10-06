6a58b66db9226863468826ed0e7c105385d172ae
Mohibul-alom <mohibulalom@outlook.es>
2021-10-06 13:04:07 +0200

    FEAT: added user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,27 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({nullable: false})
+    name: string;
+
+    @Column({nullable: false})
+    email: string;
+
+    @Column({nullable: false})
+    genere: string;
+
+    @Column({nullable: false})
+    education: string;
+
+    @Column({nullable: false})
+    birhyear: string;
+
+    @Column({nullable: true}) 
+    country: string;
+
+}
